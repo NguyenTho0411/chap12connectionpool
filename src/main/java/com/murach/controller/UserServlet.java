66dbdee612c304a446d5d2b84bbd61c89aa8ee5e
unknown <nguyentho0411@gmail.com>
2024-11-14 01:44:31 +0700

    s

---
+++ src/main/java/com/murach/controller/UserServlet.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.murach.controller;
+
+import com.murach.data.UserDB;
+import com.murach.entity.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "UserServlet", urlPatterns = {"/"})
+public class UserServlet extends HttpServlet {
+
+   @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+
+        String url = "/index.jsp";
+        
+        // get current action
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "display_users";  // default action
+        }
+        
+        // perform action and set URL to appropriate page
+        if (action.equals("display_users")) {            
+            // get list of users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);
+        } 
+        else if (action.equals("display_user")) {
+            String emailAddress = request.getParameter("email");
+            User user = UserDB.selectUser(emailAddress);
+            session.setAttribute("user", user);
+            url = "/user.jsp";
+        }
+        else if (action.equals("update_user")) {
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+
+            // get and update user
+            User user = (User) session.getAttribute("user");        
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            UserDB.update(user);
+
+            // get and set updated users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);            
+        }
+        else if (action.equals("delete_user")) {
+            // get the user
+            String email = request.getParameter("email");
+            User user = UserDB.selectUser(email);
+            
+            // delte the user
+            UserDB.delete(user);
+            
+            // get and set updated users
+            ArrayList<User> users = UserDB.selectUsers();            
+            request.setAttribute("users", users);            
+        }
+        
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }    
+    
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
